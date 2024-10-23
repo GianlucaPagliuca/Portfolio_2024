626830859d7feea4820319636ad2061f065b6ec8
Gianluca Pagliuca <77861286+GianlucaPagliuca@users.noreply.github.com>
2024-10-23 23:11:24 +0100

    Inital Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./app.scss";
+
+const App = () => {
+  return <div>
+    <section>Hero</section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Parallax</section>
+    <section>Portfolio</section>
+    <section>Contact</section>
+  </div>;
+};
+
+export default App;
